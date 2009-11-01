6b2483ef458ee978c69948b6b2820a884db41311
Hiromi Ishii <mr_konn@jcom.home.ne.jp>
2009-11-01 22:14:49 +0900

    adding List support (Not completed; DOESN'T WORK!)

--- lib/twitter.rb
+++ lib/twitter.rb
@@ -3,8 +3,43 @@ require 'enumerator'
 require 'rexml/document'
 require 'uri'
 require 'net/http'
+require 'time'
 
 Net::HTTP.version_1_2
+
+  def unescape(string)
+    string.gsub(/&(amp|quot|gt|lt|\#[0-9]+|\#x[0-9A-Fa-f]+);/n) do
+      match = $1.dup
+      case match
+      when 'amp'                 then '&'
+      when 'quot'                then '"'
+      when 'gt'                  then '>'
+      when 'lt'                  then '<'
+      when /\A#0*(\d+)\z/n       then
+        if Integer($1) < 256
+          Integer($1).chr
+        else
+          if Integer($1) < 65536 and ($KCODE[0] == ?u or $KCODE[0] == ?U)
+            [Integer($1)].pack("U")
+          else
+            "&##{$1};"
+          end
+        end
+      when /\A#x([0-9a-f]+)\z/ni then
+        if $1.hex < 256
+          $1.hex.chr
+        else
+          if $1.hex < 65536 and ($KCODE[0] == ?u or $KCODE[0] == ?U)
+            [$1.hex].pack("U")
+          else
+            "&#x#{$1};"
+          end
+        end
+      else
+        "&#{match};"
+      end
+    end
+  end
 
 module Identity
 
@@ -21,47 +56,159 @@ end
 class Twitter
   attr_reader :user, :pass
 
-  def self.public_timeline
-    new("", "").public_timeline
-  end
-
-  def self.define_get(kind)
-    define_method("#{kind}_get"){|*arg|
-      mtd, args, auth = arg
-      auth = true if auth.nil?
-      get("#{kind}/#{mtd}.xml", args, auth)
-    }
-  end
-
+  class <<self
+    def public_timeline
+      new("", "").public_timeline
+    end
+    private
   
-  def self.define_id_proc(name, rclass, rproc, arg)
-    define_method(name){|id|
-      id = id[:id] if id.is_a?(Hash)
-      id = id.id if id.is_a?(Identity)
-      rclass.new __send__(rproc, "#{arg}/#{id}", *arg)
-    }
+    def define_get(kind)
+      define_method("#{kind}_get"){|*arg|
+        mtd, args, auth = arg
+        auth = true if auth.nil?
+        get("#{kind}/#{mtd}.xml", args, auth)
+      }
+    end
+  
+    
+    def define_id_proc(name, rclass, rproc, arg)
+      define_method(name){|id|
+        id = id[:id] if id.is_a?(Hash)
+        id = id.id if id.is_a?(Identity)
+        rclass.new __send__(rproc, "#{arg}/#{id}", *arg)
+      }
+    end
+  
+    def define_timeline(prefix, auth=true)
+      define_method("#{prefix}_timeline"){|*opts|
+        if opts.empty?
+          opts = {}
+        else
+          opts = opts[0]
+          opts = {:id => opts} unless opts.is_a?(Hash)
+        end
+        src = statuses_get("#{prefix}_timeline", opts, auth)
+        proc_statuses(src)
+      }
+    end
+  
+    def define_proc_structs(parent, child)
+      kn = child.split("_").map{|a| a.capitalize}.join
+      define_method("proc_#{parent.gsub(/-/,'_')}"){|src|
+        doc = REXML::Document.new(src)
+        stats = doc.enum_for(:each_element, "#{parent}/#{child}")
+        return stats.map{|st|  self.class.const_get(kn).new(st)}
+      }
+    end
+    
+    def define_struct(name, *attrs, &init)
+      k = Class.new
+      k.__send__(:attr_reader, *attrs)
+      k.__send__(:define_method, :initialize){|arg|
+        case arg
+        when REXML::Element
+          init_with_elem(arg)
+        when String
+          stat = REXML::Document.new(arg)
+          init_with_elem(stat.elements[1])
+        when Hash
+          attrs.each{|i|
+            instance_variable_set("@#{i.to_s}", arg[i.intern])
+          }
+        when Array
+          arg.each_with_index{|v, i|
+            instance_variable_set("@#{attrs[i]}", i)
+          }
+        end
+      }
+      
+      k.__send__(:define_method, :init_with_elem){|elem|
+        attrs.each{|i|
+          el = elem.elements[i.to_s]
+          el = el.text if el
+          instance_variable_set("@#{i.to_s}", el)
+        }
+        init_sub(elem)
+      }
+      k.__send__(:define_method, :init_sub, &init)
+      const_set(name, k)
+    end
+
+  end # class <<self
+
+  define_struct(:Status, :created_at, :id, :text, :source, 
+                :in_reply_to, :in_reply_to_user_id, :user){ |elem|
+    @created_at = Time.parse(@created_at.to_s) if @created_at
+    @id = @id.to_i
+    @user = User.new(elem.elements["user"])
+    @text = unescape(@text)
+  }
+
+  define_struct(:DirectMessage, :text, :sender_id, :recipient_id, :created_at,
+                :sender_screen_name, :recipient_screen_name, :id
+  ) do |elem|
+    @sender_id = @sender_id.to_i
+    @recipient_id = @recipient_id.to_i
+    @created_at &&= Time.parse(@created_at)
   end
 
-  def self.define_timeline(prefix, auth=true)
-    define_method("#{prefix}_timeline"){|*opts|
-      if opts.empty?
-        opts = {}
-      else
-        opts = opts[0]
-        opts = {:id => opts} unless opts.is_a?(Hash)
+  define_struct(:User,
+    :id, :name, :screen_name, :location, :description, 
+    :profile_image_url, :url, :protected, :profile_background_color, 
+    :profile_text_color, :profile_link_color, :profile_sidebar_fill_color, 
+    :profile_sidebar_border_color, :friends_count, :followers_count, 
+    :favourites_count, :statuses_count, :status
+  ) do |elem|
+    ints = %w[
+      @id @statuses_count @friends_count @followers_count @favourites_count @statuses_count
+    ]
+    ints.each{|v|
+      instance_variable_set(v, instance_variable_get(v).to_i)
+    }
+    @status = Status.new(elem.elements["status"])
+    @protected = @protected.downcase != "false"
+  end
+
+  define_struct(:List,
+    :id, :name, :full_name, :slug, :subscriber_count, :member_count, :uri, :mode, :user
+  ) do |elem|
+    ints = %w[ @id @subscriber_count @member_count ].each{|v|
+      instance_variable_set(v, instance_variable_get(v).to_i)
+    }
+    @user = User.new( elem.elements["user"] )
+    @mode = @mode.downcase.intern
+    
+    @ul = nil
+  end
+  
+  class List
+    def private?; @mode == :private end
+    def public?; !private? end
+
+    [:users, :next_id, :prev_id].each {|mtd|
+      define_method(mtd) do
+        unless instance_variable_get("@#{mtd}")
+          ul()
+        end
+        instance_variable_get("@#{mtd}")
       end
-      src = statuses_get("#{prefix}_timeline", opts, auth)
-      proc_statuses(src)
     }
+    
+    private
+    def ul
+      mem = get("/mr_konn/shadow-tl/members.xml", {}, true).body
+      @ul ||= UsersList.new(mem)
+      @users = @ul.users
+      @next_id = @ul.next_cursor
+      @prev_id = @ul.previous_cursor
+    end
   end
 
-  def self.define_proc_structs(parent, child)
-    kn = child.split("_").map{|a| a.capitalize}.join
-    define_method("proc_#{parent.gsub(/-/,'_')}"){|src|
-      doc = REXML::Document.new(src)
-      stats = doc.enum_for(:each_element, "#{parent}/#{child}")
-      return stats.map{|st|  self.class.const_get(kn).new(st)}
-    }
+  define_struct(:UsersList,  :users, :next_cursor, :previous_cursor) do |elem|
+    stats = elem.elements["users"].enum_for(:each_element, "user")
+    @users = stats.map{|st|  User.new(st)}
+    @previous_cursor = @previous_cursor.to_i
+    @next_cursor = @next_cursor.to_i
   end
 
   def initialize(user, pass)
@@ -103,15 +250,20 @@ class Twitter
   end
   private :get
 
-  define_get(:direct_messages)
-  define_get(:statuses)
-  define_get(:favourings)
-  define_get(:notifications)
-  define_get(:friendships)
+  def lists(user=@user)
+    body = get("/#{user}/lists.xml", {}, true)
+    REXML::Document.new(body).enum_for(:each_element, "/lists_list/lists/list").map{|a| List.new a}
+  end
 
-  define_timeline(:public, false)
-  define_timeline(:friends)
-  define_timeline(:user)
+  define_get :direct_messages
+  define_get :statuses
+  define_get :favourings
+  define_get :notifications
+  define_get :friendships
+
+  define_timeline :public
+  define_timeline :friends
+  define_timeline :user
 
   def show(id)
     src = get("users/show/#{id}.xml", {}, true)
@@ -135,6 +287,7 @@ class Twitter
   define_proc_structs("users", "user")
   define_proc_structs("direct-messages", "direct_message")
   define_proc_structs("statuses", "status")
+  define_proc_structs("lists_list", "lists/list")
 
   def friends(id=@user)
     id = id[:id] if id.is_a?(Hash)
@@ -232,72 +385,4 @@ class Twitter
   define_id_proc(:im_follow, User, :notifications_get, "follow")
   define_id_proc(:im_follow, User, :notifications_get, "leave")
 
-end
-
-require "time"
-class <<Twitter
-  def define_struct(name, *attrs, &init)
-    k = Class.new
-    k.__send__(:attr_reader, *attrs)
-    k.__send__(:define_method, :initialize){|arg|
-      case arg
-      when REXML::Element
-        init_with_elem(arg)
-      when String
-        stat = REXML::Document.new(arg)
-        init_with_elem(stat.elements[1])
-      when Hash
-        attrs.each{|i|
-          instance_variable_set("@#{i.to_s}", arg[i.intern])
-        }
-      when Array
-        arg.each_with_index{|v, i|
-          instance_variable_set("@#{attrs[i]}", i)
-        }
-      end
-    }
-    
-    k.__send__(:define_method, :init_with_elem){|elem|
-      attrs.each{|i|
-        el = elem.elements[i.to_s]
-        el = el.text if el
-        instance_variable_set("@#{i.to_s}", el)
-      }
-      init_sub(elem)
-    }
-    k.__send__(:define_method, :init_sub, &init)
-    const_set(name, k)
-  end
-end
-
-class Twitter
-  define_struct(:Status, :created_at, :id, :text, :source, 
-                :in_reply_to, :in_reply_to_user_id, :user){|elem|
-    @created_at = Time.parse(@created_at.to_s) if @created_at
-    @id = @id.to_i
-    @user = User.new(elem.elements["user"])
-  }
-  
-  define_struct(:DirectMessage, :text, :sender_id, :recipient_id, :created_at,
-                :sender_screen_name, :recipient_screen_name, :id){|elem|
-    @sender_id = @sender_id.to_i
-    @recipient_id = @recipient_id.to_i
-    @created_at &&= Time.parse(@created_at)
-  }
-
-  define_struct(:User,
-    :id, :name, :screen_name, :location, :description, 
-    :profile_image_url, :url, :protected, :profile_background_color, 
-    :profile_text_color, :profile_link_color, :profile_sidebar_fill_color, 
-    :profile_sidebar_border_color, :friends_count, :followers_count, 
-    :favourites_count, :statuses_count, :status){|elem|
-    ints = %w[
-      @id @statuses_count @friends_count @followers_count @favourites_count @statuses_count
-    ]
-    ints.each{|v|
-      instance_variable_set(v, instance_variable_get(v).to_i)
-    }
-    @status = Status.new(elem.elements["status"])
-    @protected = @protected.downcase != "false"
-  }
 end
